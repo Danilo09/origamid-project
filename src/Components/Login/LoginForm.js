60f56ed8aa43013ab3025fec9126e2ba256b25cd
Danilo <danilo.nascimento2@hotmail.com>
2022-12-28 11:36:57 -0300

    Validate input errors changing fields

--- src/Components/Login/LoginForm.js
+++ src/Components/Login/LoginForm.js
@@ -1,32 +1,38 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import useForm from '../../Hooks/useForm'
 import Button from '../Forms/Button'
 import Input from '../Forms/Input'
 
 function LoginForm() {
+    const username = useForm();
+    const password = useForm();
 
     function handleSubmit(event) {
         event.preventDefault()
-        fetch('https://dogsapi.origamid.dev/json/jwt-auth/v1/token', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({
+
+        if (username.validate() && password.validate()) {
+            fetch('https://dogsapi.origamid.dev/json/jwt-auth/v1/token', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({
+                })
+            }).then(response => {
+                console.log(response)
+                return response.json();
+            }).then(json => {
+                console.log(json)
             })
-        }).then(response => {
-            console.log(response)
-            return response.json();
-        }).then(json => {
-            console.log(json)
-        })
+        }
     }
     return (
         <section>
             <h1>Login</h1>
             <form action='' onSubmit={handleSubmit}>
-                <Input label="User" type="text" name="username" />
-                <Input label="Password" type="password" name="password" />
+                <Input label="User" type="text" name="username" {...username} />
+                <Input label="Password" type="password" name="password" {...password} />
                 <Button>Entrar</Button>
             </form>
             <Link to="/login/create">Cadastro</Link>
