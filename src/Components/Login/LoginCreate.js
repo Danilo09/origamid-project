dd52c9c7fc078a1d46c114a3482bd4576b776184
Danilo <danilo.nascimento2@hotmail.com>
2022-12-30 16:03:26 -0300

    Form Create Login and endpoint

--- src/Components/Login/LoginCreate.js
+++ src/Components/Login/LoginCreate.js
@@ -1,8 +1,38 @@
 import React from 'react'
+import useForm from '../../Hooks/useForm';
+import Button from '../Forms/Button';
+import Input from '../Forms/Input';
+import { USER_POST } from '../../api';
+import { UserContext } from '../../userContext';
 
 function LoginCreate() {
+    const username = useForm();
+    const email = useForm('email');
+    const password = useForm();
+
+    const { userLogin } = React.useContext(UserContext);
+
+    async function handleSubmit(event) {
+        event.preventDefault();
+        const { url, options } = USER_POST({
+            username: username.value,
+            email: email.value,
+            password: password.value,
+        })
+        const response = await fetch(url, options);
+        if (response.ok) userLogin(username.value, password.value)
+
+    }
     return (
-        <div>Criar Login </div>
+        <section className='animeLeft'>
+            <h1 className='title'>Cadastre-se</h1>
+            <form onSubmit={handleSubmit}>
+                <Input label='Usuário' type='text' name="username" {...username} />
+                <Input label='E-mail' type='email' name="email" {...email} />
+                <Input label='Password' type='password' name="password" {...password} />
+                <Button>Cadastrar</Button>
+            </form>
+        </section>
     )
 }
 
