e61be84f0efe08a6cb28df840760f6f47860a062
Danilo <danilo.nascimento2@hotmail.com>
2023-01-01 16:31:42 -0300

    Hook: useFetch to create a usefull fetch on app

--- src/Components/Login/LoginCreate.js
+++ src/Components/Login/LoginCreate.js
@@ -4,6 +4,8 @@ import Button from '../Forms/Button';
 import Input from '../Forms/Input';
 import { USER_POST } from '../../api';
 import { UserContext } from '../../userContext';
+import useFetch from '../../Hooks/useFetch';
+import Error from '../Helper/Error';
 
 function LoginCreate() {
     const username = useForm();
@@ -11,6 +13,7 @@ function LoginCreate() {
     const password = useForm();
 
     const { userLogin } = React.useContext(UserContext);
+    const { loading, error, request } = useFetch();
 
     async function handleSubmit(event) {
         event.preventDefault();
@@ -19,7 +22,7 @@ function LoginCreate() {
             email: email.value,
             password: password.value,
         })
-        const response = await fetch(url, options);
+        const { response } = await request(url, options);
         if (response.ok) userLogin(username.value, password.value)
 
     }
@@ -30,7 +33,9 @@ function LoginCreate() {
                 <Input label='Usuário' type='text' name="username" {...username} />
                 <Input label='E-mail' type='email' name="email" {...email} />
                 <Input label='Password' type='password' name="password" {...password} />
-                <Button>Cadastrar</Button>
+                {loading ? <Button disabled>Cadastrando...</Button> : <Button>Cadastrar</Button>}
+                <Error error={error} />
+                <Error>{error}</Error>
             </form>
         </section>
     )
