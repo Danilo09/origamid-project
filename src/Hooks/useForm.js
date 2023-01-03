ff6e0283392d7b4dc06ace94bdd46f04892d8fb3
Danilo <danilo.nascimento2@hotmail.com>
2023-01-03 14:23:25 -0300

    How to create photo post

--- src/Hooks/useForm.js
+++ src/Hooks/useForm.js
@@ -4,6 +4,10 @@ const types = {
     email: {
         regex: /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
         message: 'Preencha um e-mail válido'
+    },
+    number: {
+        regex: /^\d+$/,
+        message: 'Utilize apenas números'
     }
 }
 
