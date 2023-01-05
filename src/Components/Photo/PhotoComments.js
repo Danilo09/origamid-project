c40ee9d729612c677fb8ca21e4809d5a107d55b5
Danilo <danilo.nascimento2@hotmail.com>
2023-01-05 16:45:38 -0300

    How to style comments form

--- src/Components/Photo/PhotoComments.js
+++ src/Components/Photo/PhotoComments.js
@@ -5,10 +5,15 @@ import styles from './PhotoComments.module.css'
 
 function PhotoComments(props) {
     const [comments, setComments] = React.useState(() => props.comments)
+    const commentsSection = React.useRef(null);
     const { login } = React.useContext(UserContext)
+
+    React.useEffect(() => {
+        commentsSection.current.scrollTop = commentsSection.current.scrollHeight;
+    }, [comments])
     return (
         <>
-            <ul className={styles.comment}>
+            <ul ref={commentsSection} className={styles.comments}>
                 {comments.map(comment =>
                     <li key={comment.comment_ID}>
                         <b>{comment.comment_author}: </b>
--- src/Components/Photo/PhotoCommentsForm.js
+++ src/Components/Photo/PhotoCommentsForm.js
@@ -3,6 +3,7 @@ import { COMMENT_POST } from '../../api';
 import { ReactComponent as Enviar } from '../../Assets/enviar.svg';
 import useFetch from '../../Hooks/useFetch';
 import Error from '../Helper/Error';
+import styles from './PhotoCommentsForm.module.css'
 
 function PhotoCommentsForm({ id, setComments }) {
     const [comment, setComment] = React.useState('');
@@ -18,14 +19,15 @@ function PhotoCommentsForm({ id, setComments }) {
         }
     }
     return (
-        <form onSubmit={handleSubmit}>
+        <form className={styles.form} onSubmit={handleSubmit}>
             <textarea
+                className={styles.textarea}
                 id="comment"
                 name="comment"
                 placeholder='Comente...'
                 value={comment}
                 onChange={({ target }) => setComment(target.value)} />
-            <button>
+            <button className={styles.button}>
                 <Enviar />
             </button>
             <Error error={error} />
