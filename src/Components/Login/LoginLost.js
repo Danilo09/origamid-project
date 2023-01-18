605f7af3ffac45edd203f268ee55c3af364fb599
Danilo <danilo.nascimento2@hotmail.com>
2023-01-18 10:43:46 -0300

    Creating a reset and lost page login

--- src/Components/Login/LoginLost.js
+++ src/Components/Login/LoginLost.js
@@ -1,8 +1,37 @@
 import React from 'react'
+import Button from '../Forms/Button'
+import Input from '../Forms/Input'
+import useForm from '../../Hooks/useForm';
+import useFetch from '../../Hooks/useFetch';
+import { PASSWORD_LOST } from '../../api';
+import Error from '../Helper/Error';
 
 function LoginLost() {
+    const login = useForm();
+    const { data, loading, error, request } = useFetch();
+
+    async function handleSubmit(event) {
+        event.preventDefault()
+        if (login.validate()) {
+            const { url, options } = PASSWORD_LOST({ login: login.value, url: window.location.href.replace('lost', 'reset') })
+            const { json } = await request(url, options)
+            console.log(json)
+        }
+    }
+
     return (
-        <div>LoginLost</div>
+        <section>
+            <h1 className='title'>Perdeu a senha?</h1>
+            {data ? (<p style={{ color: '#4c1' }}>{data}</p>) :
+                <form onSubmit={handleSubmit}>
+                    <Input label="Email / Usuário" type="text" name="login" {...login} />
+                    {loading ? <Button disabled>Enviando...</Button> : <Button>Enviar E-mail</Button>}
+
+                </form>
+            }
+
+            <Error error={error} />
+        </section>
     )
 }
 
