120be360e3e5a72e709737ae6b308b03e9b4ef28
Danilo <danilo.nascimento2@hotmail.com>
2023-01-18 09:18:39 -0300

    Creating PropType Require in user js

--- src/Components/Feed/Feed.js
+++ src/Components/Feed/Feed.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import FeedModal from './FeedModal'
 import FeedPhotos from './FeedPhotos'
+import PropTypes from 'prop-types'
 
 function Feed({ user }) {
     const [modalPhoto, setModalPhoto] = React.useState(null);
@@ -36,6 +37,14 @@ function Feed({ user }) {
 
         </div>
     )
+}
+
+Feed.defaultProps = {
+    user: 0,
+}
+
+Feed.propTypes = {
+    user: PropTypes.oneOfType([PropTypes.string.isRequired, PropTypes.number.isRequired]).isRequired,
 }
 
 export default Feed
--- src/Components/Feed/FeedPhotos.js
+++ src/Components/Feed/FeedPhotos.js
@@ -16,7 +16,6 @@ function FeedPhotos({ page, user, setModalPhoto, setInfinite }) {
             const { url, options } = PHOTOS_GET({ page, total, user })
             const { response, json } = await request(url, options)
             if (response && response.ok && json.length < total) setInfinite(false)
-            console.log('Request:', json)
         }
         fetchPhotos();
     }, [request, page, user, setInfinite])
